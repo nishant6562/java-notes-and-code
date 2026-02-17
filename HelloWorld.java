7c284bf8304de2a3bba21908032514d776e137aa
Nishant <158156561+nishant6562@users.noreply.github.com>
2026-02-17 23:29:45 +0530

    code

---
+++ HelloWorld.java
@@ -0,0 +1,27 @@
+public class HelloWorld {
+    
+    // Your program begins with a call to main()
+    public static void main(String[] args)
+    {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}
+    public static void main(String[] args) {
+        
+        // Declaring and initializing variables
+        
+        // Integer variable  
+        int age = 25;       
+        
+        // String variable  
+        String name = "GeeksforGeeks"; 
+        
+        // Double variable 
+        double salary = 50000.50;     
+
+        // Displaying the values of variables
+        System.out.println("Age: " + age);          
+        System.out.println("Name: " + name);        
+        System.out.println("Salary: " + salary);
+    }
